bf4467f4a798779424b4b649a33acb6863c03cac
AlanDamotte <Alan.damotte@e.ujf-grenoble.fr>
2014-04-24 14:20:35 +0200

    Modif apportées à la class Java

--- src/jus/aoo/boole/composant/Et.java
+++ src/jus/aoo/boole/composant/Et.java
@@ -1,10 +1,13 @@
 package jus.aoo.boole.composant;
+
 import jus.aoo.boole.port.*;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Et extends Transformateur {
-//	public Et(int numcomposant,){
-//		super("Et",numcomposant,1,2,);
-//	}
+	public Et(int numcomposant,List<Sortie> list_sort, List<Entree> list_ent){
+		super("Et",numcomposant,1,2,list_sort,list_ent);
+	}
 	 boolean boole_et(Entree entree1, Entree entree2){
 		return (entree1.get_etat() && entree2.get_etat());
 	}
