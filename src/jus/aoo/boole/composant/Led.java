c0af4ec55ed2f3ac4ae93a3d09b7093d87adb62c
RomainBadamoBarthelemy <https://github.com/AlanDamotte/Boole.git>
2014-04-29 18:35:01 +0200

    Accesseur de led ajoute

--- src/jus/aoo/boole/composant/Led.java
+++ src/jus/aoo/boole/composant/Led.java
@@ -6,23 +6,16 @@ import jus.aoo.boole.port.Port;
 
 public class Led extends $Recepteur{
 	
-	//Niveau n'a pas sa place ici
 	private NiveauVisuel ampoule;
 	
 	public Led(){
 		super("Led",new Port[1]);
 	}
 	
-	/*public Niveau get_niveau(){
-		return new Niveau(niveau);
-	}*/
+	public NiveauVisuel get(){
+		return this.ampoule;
+	}
 	
-	//Operer
-	//if(entree.get_etat()){this.niveau=Niveau.Allume;}
-	//else{this.niveau=Niveau.Eteint;}
-	//super.ent_list.add(entree);
-	
-	// A MODIFIER
 	public void operer(){
 		if(ent_tab[0].get_etat()==Niveau.Haut){
 			this.ampoule=NiveauVisuel.Allume;
